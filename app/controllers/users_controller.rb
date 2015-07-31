adf515f29808d33eed9d18c520a9edcd1e197ddc
Mallory Bucell <m.f.bucell@gmail.com>
2015-07-31 19:21:17 -0400

    added views and routes for creating new user, viewing user index and individual user show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.add_user(create_user_params)
+    redirect_to user_path(user.id)
+  end
+
+  def edit
+    @user = User.lookup_user(user_lookup_params)
+  end
+
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.lookup_user(user_lookup_params)
+  end
+
+  private
+
+  def user_lookup_params
+    #TODO validate and flash messages/error handling
+    params.permit(:id)
+  end
+
+  def create_user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :birthdate)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #TODO remove registerable?
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.lookup_user user_id
+    User.find(user_id[:id].to_i)
+  end
+
+  def self.add_user user_params
+    User.create!(email: user_params[:email], password: user_params[:password], first_name: user_params[:first_name], last_name: user_params[:last_name], birthdate: user_params[:birthdate])
+  end
 end
