c14d4b0ef6667ede62aff16da6101615d9a8c790
Evan Light <evan@tripledogdare.net>
2015-08-01 15:30:49 -0400

    Replace permission strings with snakecase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  PERMISSIONS = [
-    "Annotate worksheets",
-    "Manage curriculum",
-    "Manage permissions"
+  PERMISSIONS = %w[
+    annotate_worksheets
+    manage_curriculum
+    manage_permissions
   ]
 end
