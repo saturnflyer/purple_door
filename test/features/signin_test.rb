a6521ac0783c60500e320796ce6ac3a1ca04dbce
Craig Zaccaro <czaccaro@live.com>
2015-08-02 12:13:06 -0400

    Finish basic sign-in test

--- test/features/signin_test.rb
+++ test/features/signin_test.rb
@@ -1,10 +1,16 @@
 require "test_helper"
 
 feature "User sign in" do
-  scenario "user enters valid sign-in credentials" do
+  scenario "user signs in with valid credentials" do
     visit '/users/sign_in'
 
-    page.must_have_content "Home"
+    within '.new_user' do
+      fill_in 'user_email',    with: 'user@aol.com'
+      fill_in 'user_password', with: 'abc1234567'
+      click_button 'Log in'
+    end
+
+    page.must_have_content " "
     page.wont_have_content "Sign in"
   end
 end
