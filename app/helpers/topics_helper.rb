4dbd06c03ffe20543b77273628433b7ed790ca65
Evan Light <evan@tripledogdare.net>
2015-08-02 14:54:42 -0400

    Fixed worksheet completed counter

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,8 +1,7 @@
 module TopicsHelper
   def worksheets_complete_for_topic_by_user(topic, user)
-    rel = topic.user_worksheets
-    total_count = rel.count
-    user_count  = rel.where(user: user).count
+    total_count = topic.worksheets.count
+    user_count  = topic.user_worksheets.where(user: user).count
 
     if total_count != 0 && user_count != 0
       "(#{user_count}/#{total_count})"
