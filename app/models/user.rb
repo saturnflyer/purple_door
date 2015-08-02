eb5ac0cd82b7c78c9500844e44c72129e38c3c21
Evan Light <evan@tripledogdare.net>
2015-08-02 11:49:53 -0400

    Add predicates for admin and employee

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,14 @@ class User < ActiveRecord::Base
   def edit_info! edit_params
     self.update!(email: edit_params[:email], password: edit_params[:password], first_name: edit_params[:first_name], last_name: edit_params[:last_name], birthdate: edit_params[:birthdate])
   end
-  
+
   has_many :events
+
+  def admin?
+    permissions.any?
+  end
+
+  def employee?
+    permissions.none?
+  end
 end
