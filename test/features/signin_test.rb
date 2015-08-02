2d8304ecfee62da7ad1e28e1c417fb25f0452c29
Craig Zaccaro <czaccaro@live.com>
2015-08-02 11:17:13 -0400

    Changes to sign in test

--- test/features/signin_test.rb
+++ test/features/signin_test.rb
@@ -4,7 +4,7 @@ feature "User sign in" do
   scenario "user enters valid sign-in credentials" do
     visit '/users/sign_in'
 
-    page.must_have_content " "
+    page.must_have_content "Home"
     page.wont_have_content "Sign in"
   end
 end
