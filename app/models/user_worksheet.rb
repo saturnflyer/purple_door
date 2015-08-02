a76fc0da375e4fdb20167d6024bad0a5a2e648a4
Marius Pop <marius@mlpinit.com>
2015-08-02 14:17:17 -0400

    add conditional edit worksheet based on user

--- app/models/user_worksheet.rb
+++ app/models/user_worksheet.rb
@@ -4,4 +4,13 @@ class UserWorksheet < ActiveRecord::Base
   has_many :question_answers
 
   accepts_nested_attributes_for :question_answers, reject_if: proc { |attr| attr[:description].blank? }
+
+  def status
+    if worksheet.questions.count == question_answers.count
+      'complete' 
+    else
+      'incomplete'
+    end
+  end
+
 end
