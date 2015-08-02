f5b1233d5f936a6b3c7bc2b40000a73ac54a5a8e
Evan Light <evan@tripledogdare.net>
2015-08-02 12:00:30 -0400

    Adds (X/Y) next to topic as an indication of progress

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,10 +2,11 @@ class Topic < ActiveRecord::Base
   belongs_to :curriculum
   has_many :worksheets, dependent: :destroy
   has_many :documents, dependent: :destroy
+  has_many :user_worksheets, through: :worksheets
   validates :name, presence: true
 
   scope :top_level, ->{ where(ancestry: nil)}
 
   has_ancestry cache_depth: true
-  
+
 end
